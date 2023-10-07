344d2a68f4770e69f29c9034bd610273941ce04a
kales <kales@SHRUTI0726>
2023-10-07 14:54:29 +0530

    trying project

---
+++ src/basic/A.java
@@ -0,0 +1,33 @@
+package basic;
+
+import java.util.Scanner;
+
+public class A {
+	int a=10;
+	int b=20;
+	int c;
+	
+	public void add()
+	{
+		  c=a+b;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+		
+		Scanner sc=new Scanner(System.in);
+		
+		     int a= sc.nextInt();
+		
+		
+		
+		
+	}
+
+	
+	
+	
+}
